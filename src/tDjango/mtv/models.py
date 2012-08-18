1653f5897dbd3636e08cc1e93ffc990eefb873d5
zhuyijuan <zhuyijuan_lh@sohu.com>
2012-08-18 19:33:46 +0800

    'modify'

--- src/tDjango/mtv/models.py
+++ src/tDjango/mtv/models.py
@@ -5,27 +5,61 @@ Created on 2012-8-15
 '''
 from django.db import models
 class Publisher(models.Model):
-    name=models.CharField(max_length=30)
-    address=models.CharField(max_length=50)
-    email=models.EmailField(max_length=30)
-    website=models.URLField()
+    name = models.CharField(max_length=30)
+    address = models.CharField(max_length=50)
+    email = models.EmailField(max_length=30)
+    website = models.URLField()
     def __unicode__(self):
         return self.name
     @classmethod
-    def create(cls,name,addr):
-        pub=cls(name=name,address=addr)
+    def create(cls, name, addr):
+        pub = cls(name=name, address=addr)
         return pub
         
     
     
     
 class Author(models.Model):
-    first_name=models.CharField(max_length=10)
-    last_name=models.CharField(max_length=30)
-    email=models.EmailField()
+    first_name = models.CharField(max_length=10)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField()
+    @classmethod
+    def authorInstance(cls, fname, lname, email):
+        a = cls(first_name=fname, last_name=lname, email=email)
+        return a
+    def __unicode__(self):
+        return self.first_name
+    
+    
+    
     
 class Book(models.Model):
-    title=models.CharField(max_length=100)
-    authors=models.ManyToManyField(Author)
-    publisher=models.ForeignKey(Publisher);
-    date=models.DateField()                              
+    title = models.CharField(max_length=100)
+    authors = models.ManyToManyField(Author)
+    publisher = models.ForeignKey(Publisher);
+    date = models.DateField()     
+    @classmethod
+    def bookInstance(cls, title, publisher, date):
+        b = cls(title=title,  publisher=publisher, date=date)
+        return b  
+    def __unicode__(self):
+        return self.title
+    
+    
+    
+    
+class Person(models.Model):
+    #2tuples
+   SHIRT_SIZES = (
+        ('s', 'small'),
+        ('m', 'medium'),
+        ('l', 'large'),
+    ) 
+   name = models.CharField(max_length=20, unique=True)
+   shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZES)
+   @classmethod
+   def personInstance(cls, name, shirt_size):
+       p = cls(name=name, shirt_size=shirt_size)
+       return p
+       
+                         
--- src/tDjango/mtv/testdb.py
+++ src/tDjango/mtv/testdb.py
@@ -1,16 +1,46 @@
-'''
-Created on 2012-8-15
-
-@author: dell
-'''
-from tDjango.mtv.models import Publisher
+# _*_ coding: utf-8 _*_
+from tDjango.mtv.models import Publisher,Person,Author,Book
+from django.utils.datetime_safe import date
 if __name__ == '__main__':
-    p1=Publisher(name='emily1',address='emily1.home',email='emily1@s.com')
-   # p1.save()
-    p_list=Publisher.objects.all()
-   # print p_list
+    p1=Publisher(name='emilyPublisher',address='emily1Publisher.home',email='emilyPublisher@s.com')
+    p1.save()
+    a=Author.authorInstance("emily","zhu","emily@s.com")
+    a.save()
+    a1=Author.authorInstance("emily1","zhuzhu","emily.zhu@s.com")
+    a1.save()
+    b=Book(title="emily's the first book",publisher=p1,date=date.today()) 
+     
+    #####----设置多对多关系时，必须先保存本对象，因为python用了中间表保存，所以必须先保存才能有ID入中间表
+    b.save()
+    b.authors=(a,a1)
+    b.save()
+    b2=Book.bookInstance("emily's the second book", p1, date.today())
+    b2.save()
+    b2.authors=[a1]
+    b2.save()
+   
+
+
+    #####-------many-to-one  one的一方默认一个（有many对应的class_set）many方的引用（onerelationmanage对象），
+    ##########------------取出many方的数据必须调all()才能转成集合使用
+    bl=Publisher.objects.all()[0].book_set.all()
+    for b in bl:
+       print b
+        
+    ##### many-relation_objects must invoke all()----iterator so get the value   
+    al=Book.objects.all()[0].authors.all()
+    for at in al:
+        print at    
+
     
-    p2=Publisher.create("zyj","addd")
-    print p2
-    #for p in p_list:
-       # print p
+    p=Person.personInstance('zhuyijuanq','m')
+    p.save()
+    p=Person.objects.all()[0]
+    #####----shirt_size的使用类似于java中的enum，所有在person申明中用了tuple定义了常量，
+    #####-------------get_shirt_size_display调用了tuple对应的字典中的value,数据库中存入的是 key
+    print p.shirt_size
+    print p.get_shirt_size_display()
+    
+    
+#    
+#    
